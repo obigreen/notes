17f2f2cd8e773aae07c50c4556152c792c8a8fac
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-01 19:15:45 +0300

    changed styles

--- src/recordsdirectory/english/English_Styles.ts
+++ src/recordsdirectory/english/English_Styles.ts
@@ -7,6 +7,7 @@ const TextWrapper = styled.div`
     align-items: center;
     justify-content: space-between;
     padding: 10px;
+    gap: 100px;
 `
 
 const RusWord = styled.p`
@@ -14,12 +15,15 @@ const RusWord = styled.p`
     font-size: 20px;
     font-style: normal;
     color: #1e1f22;
+    width: 50%;
 `
 const EngWord = styled.p`
     font-weight: 600;
     font-size: 20px;
     font-style: normal;
     color: #1e1f22;
+    width: 50%;
+    text-align: end;
 `
 
 export const S = {
