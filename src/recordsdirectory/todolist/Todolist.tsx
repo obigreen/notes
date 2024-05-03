d8e4a78cb690eef392e47c68f7f365dcc1a3c92b
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-03 23:31:57 +0300

    added two todo

--- src/recordsdirectory/todolist/Todolist.tsx
+++ src/recordsdirectory/todolist/Todolist.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {OneTodolist} from "./todolists/OneTodolist";
 import {TypeTitle} from "../RecordsDirectory_Style";
 import {CodeForLessons} from "./todolists/code/CodeForLessons";
+import {TwoTodolist} from "./todolists/TwoTodolist";
 
 export const Todolist = () => {
     return (
         <>
             <TypeTitle>Todolists</TypeTitle>
             <OneTodolist/>
+            <TwoTodolist/>
 
             {/*<CodeForLessons/>*/}
         </>
