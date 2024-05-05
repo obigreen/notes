41c7ed2ee49f4beb5ea68dc4c5b1645355ff38d4
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-05 18:29:34 +0300

    corrected name methods

--- src/recordsdirectory/methods/methodsarray/ObjectMethods.tsx
+++ src/recordsdirectory/methods/methodsarray/ObjectMethods.tsx
@@ -183,7 +183,7 @@ export const ObjectMethods: React.FC<MethodProps> = ({objectItems = []}) => {
 
     return (
         <NoteBlock>
-            <NotesTitle>Method Objects</NotesTitle>
+            <NotesTitle>Method Object</NotesTitle>
             <Text>
                 <S.List>
                     {objectItems.map((item, index) => (
